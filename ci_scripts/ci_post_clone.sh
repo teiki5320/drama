2a9478af6bee95f8596e270f515b88cbea8d003a
teiki5320 <teiki5320@gmail.com>
2026-05-10 09:22:43 +0200

    ci: add Xcode Cloud post-clone script (install Flutter + pub get + pod install)

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+echo "=== Install Flutter SDK ==="
+git clone --depth 1 --branch stable https://github.com/flutter/flutter.git $HOME/flutter
+export PATH="$HOME/flutter/bin:$PATH"
+
+echo "=== Precache iOS artifacts ==="
+flutter precache --ios
+
+echo "=== flutter pub get ==="
+cd $CI_PRIMARY_REPOSITORY_PATH
+flutter pub get
+
+echo "=== pod install ==="
+cd $CI_PRIMARY_REPOSITORY_PATH/ios
+pod install
